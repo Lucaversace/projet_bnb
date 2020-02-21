f787e79cbecf820e55fb6e5b00e007ca11a280a6
Lucas Bento-Versace <lucas.bentoversace@ynov.com>
2020-02-21 19:13:01 +0100

    creation class annonce

---
+++ class/ClassManager.php
@@ -0,0 +1,24 @@
+<?php
+
+Class Manager{
+
+public function connexion_bdd(){
+
+    $db = new PDO('mysql:host=localhost;dbname=projet_bnb', 'root', 'root');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- layout/footer.php
+++ layout/footer.php
@@ -1,5 +1,5 @@
 <?php ob_start();?>
 
-<h1>eden et lucas</h1>
+
 
 <?php $footer = ob_get_clean();?>
