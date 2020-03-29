932dd784c9b3b9297789cf33b32079dd51b50706
Lucas Bento-Versace <lucas.bentoversace@ynov.com>
2020-03-29 19:12:42 +0200

    menu responsive

--- views/layout/header.php
+++ views/layout/header.php
@@ -3,16 +3,19 @@ ob_start();
 ?>
 
 <nav class="navbar navbar-default bg-dark navbar-expand-lg" role="navigation">
+    
     <div class="container-fluid">
-    <a class="navbar-brand navbar-left" href="/Accueil"><img id="logo" src="img/logo.png" alt="Logo iBed"></a>
-        <div class="navbar-header">
 
+        <a class="navbar-brand navbar-left" href="/Accueil"><img id="logo" src="img/logo.png" alt="Logo iBed"></a>
+
+        <div class="navbar-header">
             <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#mobile-menu">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
         </div>
+
         <div class="collapse navbar-collapse" id="mobile-menu">
+
             <ul class="w-100 nav navbar-nav d-flex justify-content-around">
 
                 <li class="nav-item">
