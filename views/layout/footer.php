f872c88ca7cb29d919efc5af6c659defa0082a7d
ValentinJ <valentin.joassard@ynov.com>
2020-03-30 15:33:43 +0200

    color footer

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -3,8 +3,8 @@
 <div class="container-fluid">
 <footer class="bg-dark row d-flex align-items-center flex-wrap justify-content-around">
 
-      <div class="col-md-3 mt-3 d-flex flex-column align-items-center text-center">
-        <h5>Besoin d'aide ?</h5>
+      <div class="col-md-3 mt-3 d-flex flex-column align-items-center text-center ">
+        <h5>Besoin d'aide ? </h5>
         <ul class="list-unstyled text-small">
           <li><a class="text-muted" href="#">Cool stuff</a></li>
         </ul>
