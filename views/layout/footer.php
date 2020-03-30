081477727db2b4d1b55c06a178f2cec52bdfce1e
ValentinJ <valentin.joassard@ynov.com>
2020-03-30 15:40:30 +0200

    class h5 footer

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -4,14 +4,14 @@
 <footer class="bg-dark row d-flex align-items-center flex-wrap justify-content-around">
 
       <div class="col-md-3 mt-3 d-flex flex-column align-items-center text-center ">
-        <h5>Besoin d'aide ? </h5>
+        <h5 class="h5footer">Besoin d'aide ? </h5>
         <ul class="list-unstyled text-small">
           <li><a class="text-muted" href="#">Cool stuff</a></li>
         </ul>
       </div>
 
       <div class="col-md-3 mt-3 d-flex flex-column align-items-center text-center">
-        <h5>Contact</h5>
+        <h5 class="h5footer">Contact</h5>
         <ul class="list-unstyled text-small">
           <li><a class="text-muted" href="#">Resource</a></li>
           <li><a class="text-muted" href="#">Resource name</a></li>
@@ -19,7 +19,7 @@
       </div>
 
       <div class="col-md-3 mt-3 d-flex flex-column align-items-center text-center">
-        <h5>À propos</h5>
+        <h5 class="h5footer">À propos</h5>
         <ul class="list-unstyled text-small">
           <li><a class="text-muted" href="#">Team</a></li>
         </ul>
