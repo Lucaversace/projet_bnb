fd92a97991e594766daf5ead9fafcb9fabf9f66b
ValentinJ <valentin.joassard@ynov.com>
2020-04-30 16:05:54 +0200

    page search

--- views/search.php
+++ views/search.php
@@ -6,34 +6,43 @@ $title = "recherche";
 ?>
 
 <?php ob_start();?>
-
-<h1 class="h1search mt-5 ml-3"> Plus de "N" Recherche</h1>
 <style>
     img {
       border-radius: 1vw;  
       width: 100%;
     }
+    .container-fluid:hover{
+      background-color: grey;
+    }
+    a{
+      color:black !important;
+      text-decoration: none !important;
+    }
 </style>
-<div  class="row mb-3 mt-5 ml-1 border-top border-bottom border-dark " > 
-        <div class="col-md-3 mb-5 mt-5 ">
-        <img src="img/desktop/annonce/ori4.png" alt=""> 
+
+<h1 class="h1search col-md-5 mt-5 ml-3 pb-2 border-bottom border-dark"> Plus de "N" Recherche</h1>
+
+<a href="#">
+<div class="container-fluid">
+  <div  class="row mb-3 mt-5 ml-1 d-flex align-items-center " > 
+          <div class="col-sm-3 mb-5 mt-5 ">
+            <img src="img/desktop/annonce/ori4.png" alt=""> 
         </div> 
-        <div class="col-md-3 mb-4 mt-4 mr-2 border-left border-dark">
-        <h8 class="mb-2 mt-2">type d'hébergement</h8>
-        <h3  class="mb-2 mt-4">Nom de l'hébergement</h3>
-        <h5  class="mb-2 mt-5">Nombre de place</h5>
-        <h5  class="mb-2 mt-5">Prix €</h5>
+        <div class="col-sm-3  mr-sm-2 ">
+           <h8 class="pb-2">type d'hébergement</h8>
+            <h3  class="">Nom de l'hébergement</h3>
+            <h5  class="mt-md-5 mt-sm-0">Nombre de place</h5>
+            <h5  class="mt-md-5 mt-sm-0">Prix €</h5>
         </div>
-        <div class="col-md-5 mt-4 mb-4 border-left border-dark">
-        <h5  class="">Description :</h5>
-        <h8 class="">
+        <div class="d-none d-md-block col-sm-5 mb-4 ">
+            <h5  class="">Description :</h5>
+            <h8 class="">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae neque iaculis, semper elit sit amet, lobortis dolor. Duis scelerisque ex vitae sem cursus, quis imperdiet neque interdum. Aliquam laoreet rhoncus nisi nec lacinia. In interdum risus vel magna vestibulum interdum. Morbi at condimentum est. Vivamus elementum risus tellus, vehicula tempor tortor viverra luctus. 
-        </h8>
+            </h8>
         </div>
 </div>
-
-
-
+</div>
+</a>
 <?php $content = ob_get_clean();?>
 
 <?php 
